8ef687e864da4bbb0164af7dee03ec4a7caa223b
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-05-01 03:30:21 +0100

    pushing latest changes from the replit
    Co-authored-by: ArnoldPires <arnold.pires92@gmail.com>
    Co-authored-by: elloveronika <ello.veronika92@gmail.com>
    Co-authored-by: Hüseyin Beller <bellerhuseyin@gmail.com>
    Co-authored-by: jocantu <jocantu@protonmail.com>
    Co-authored-by: Ilder Velez <velez.ilder@gmail.com>

--- routes/persons.js
+++ routes/persons.js
@@ -6,12 +6,16 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
 
-router.get("/:id", ensureAuth, personsController.getPersonById);
 
-router.post("/createPerson", personsController.createPerson);
+router.get("/:id", ensureAuth, personsController.getPersonById); // /persons/:id
+
+router.post("/createPerson",  personsController.createPerson); // this is what is attaching from route/persons/createPerson
+
+router.get('/', personsController.getAllPersons)
 
 // router.put("/:id", personsController.updatePerson) TODO later
 
-router.delete("/deletePerson/:id", personsController.deletePerson);
+router.delete("/deletePerson/:id", personsController.deletePerson);//this is dynaic :id is a place holder
+
 
 module.exports = router;
