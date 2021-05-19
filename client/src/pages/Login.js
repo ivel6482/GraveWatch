e5eeef3037758166e59e7c62325a2bba0275944f
Ilder Velez <velez.ilder@gmail.com>
2021-05-19 19:21:18 -0400

    Add login form with state

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,47 @@
+import Button from '../components/UI/Button'
+import { useState } from 'react'
 export default function Login() {
-	return <h1>Login</h1>
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const checkHandlerEmail = (e) => {
+		e.preventDefault()
+		setEmail(e.target.value)
+	}
+
+	const checkHandlerPassword = (e) => {
+		e.preventDefault()
+		setPassword(e.target.value)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		alert(email)
+		alert(password)
+		setEmail('')
+		setPassword('')
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h1>Login</h1>
+			<label htmlFor='email'>Email</label>
+			<input
+				type='email'
+				id='email'
+				value={email}
+				onChange={checkHandlerEmail}
+				className='bg-gray-100 border'
+			/>
+			<label htmlFor='password'>Password</label>
+			<input
+				type='password'
+				id='password'
+				value={password}
+				onChange={checkHandlerPassword}
+				className='bg-gray-100 border'
+			/>
+			<Button type='submit'>Login</Button>
+		</form>
+	)
 }
