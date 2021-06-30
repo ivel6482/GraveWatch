c1469d5de2b57c4d95917f943d8db6a580d08a9f
Ilder Velez <velez.ilder@gmail.com>
2021-06-30 19:12:43 -0400

    profile style for mobile screens

--- public/js/main.js
+++ public/js/main.js
@@ -1,115 +1,130 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-	const submitForm = document.querySelector('#add-person-form');
-	const updateForm = document.querySelectorAll('form.update-person-form');
+document.addEventListener('DOMContentLoaded', () => {
+	const submitForm = document.querySelector('#add-person-form')
+	const updateForm = document.querySelectorAll('form.update-person-form')
 	const deleteBtn = document.querySelectorAll('.del')
 
-	if(submitForm) {
-		submitForm.addEventListener('submit', handleFormSubmit);
+	if (submitForm) {
+		submitForm.addEventListener('submit', handleFormSubmit)
 	}
 
-	if(updateForm) {
+	if (updateForm) {
 		Array.from(updateForm).forEach((el) => {
 			el.addEventListener('submit', updatePerson)
 		})
 	}
 
-	if(deleteBtn) {
-		Array.from(deleteBtn).forEach((el)=>{
+	if (deleteBtn) {
+		Array.from(deleteBtn).forEach((el) => {
 			el.addEventListener('click', deletePerson)
 		})
 	}
-});
-
+})
 
 // Show a notification if required fields haven't been filled
 function handleFormSubmit(event) {
-	const required = ['name', 'description', 'lat', 'lon', 'file'];
-	const inputs = event.target.querySelectorAll('input[name], textarea[name]');
-	const errors = [];
+	const required = ['name', 'description', 'lat', 'lon', 'file']
+	const inputs = event.target.querySelectorAll('input[name], textarea[name]')
+	const errors = []
 
-	for(const input of inputs) {
-		const isRequired = required.includes(input.name);
-		const isEmpty = input.value === '';
+	for (const input of inputs) {
+		const isRequired = required.includes(input.name)
+		const isEmpty = input.value === ''
 
-		if(isRequired && isEmpty) {
-			errors.push(input.name);
-		}
-	};
-
-	if(errors.length > 0) {
-		event.preventDefault(); // Prevent form from submitting
-		if(errors.includes('lat') || errors.includes('lon')) {
-			notify(`Error, please select a location on the map`);
-		}
-		else {
-			// Image is called 'file' so we need to replace it
-			const fileIndex = errors.indexOf('file');
-			if(fileIndex !== -1) {
-				errors[fileIndex] = 'image';
-			}
-
-			notify(`Error, the following fields cannot be empty: ${errors.join(', ')}`);
+		if (isRequired && isEmpty) {
+			errors.push(input.name)
 		}
 	}
-};
 
-function notify(text) {
-	const notification = document.querySelector('.notification');
+	if (errors.length > 0) {
+		event.preventDefault() // Prevent form from submitting
+		if (errors.includes('lat') || errors.includes('lon')) {
+			notify(`Please select a location on the map`)
+		} else {
+			// Image is called 'file' so we need to replace it
+			const fileIndex = errors.indexOf('file')
+			if (fileIndex !== -1) {
+				errors[fileIndex] = 'image'
+			}
 
-	notification.textContent = text;
-	notification.classList.add('show');
-	setTimeout(() => {
-		notification.classList.remove('show');
-	}, 5000, notification);
+			notify(`The following fields cannot be empty: ${errors.join(', ')}`)
+		}
+	}
 }
 
+function notify(text) {
+	const notification = document.querySelector('.notification')
+
+	notification.textContent = text
+	notification.classList.add(
+		'show',
+		'p-2',
+		'font-semibold',
+		'text-red-700',
+		'bg-red-100'
+	)
+	setTimeout(
+		() => {
+			notification.classList.remove(
+				'show',
+				'p-2',
+				'font-semibold',
+				'text-red-700',
+				'bg-red-100'
+			)
+			notification.textContent = ''
+		},
+		5000,
+		notification
+	)
+}
 
 // Update submitted persons
 async function updatePerson(event) {
-	event.preventDefault(); // Prevent post request
+	event.preventDefault() // Prevent post request
 
-	const fields = Array.from(event.target.querySelectorAll('input[name], textarea[name], select[name]'));
-	const personIndex = fields.findIndex(field => field.name === 'id');
-  const personId = fields.splice(personIndex, 1)[0].value;
-	const requestBody = Object.fromEntries(fields.map(field => {
-		return [field.name, field.value]
-	}))
+	const fields = Array.from(
+		event.target.querySelectorAll('input[name], textarea[name], select[name]')
+	)
+	const personIndex = fields.findIndex((field) => field.name === 'id')
+	const personId = fields.splice(personIndex, 1)[0].value
+	const requestBody = Object.fromEntries(
+		fields.map((field) => {
+			return [field.name, field.value]
+		})
+	)
 
 	try {
-    const response = await fetch(`/persons/updatePerson/${personId}`, {
-      method: 'put',
-      headers: { 'Content-type': 'application/json'},
-      body: JSON.stringify(requestBody),
-    })
-    location.replace(`/persons/${personId}`);
-  }
-  catch (err) {
-    console.error(err)
-  }
+		const response = await fetch(`/persons/updatePerson/${personId}`, {
+			method: 'put',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(requestBody),
+		})
+		location.replace(`/persons/${personId}`)
+	} catch (err) {
+		console.error(err)
+	}
 }
-
 
 // Delete submitted persons
 /*/deletePerson/:id */
-async function deletePerson(event){
-    const person = event.currentTarget.closest('[data-id]');
-    const personId = person.dataset.id;
-		const page = new URL(window.location);
+async function deletePerson(event) {
+	const person = event.currentTarget.closest('[data-id]')
+	const personId = person.dataset.id
+	const page = new URL(window.location)
 
-    try{
-        const response = await fetch(`/persons/deletePerson/${personId}`, {
-            method: 'DELETE',
-        });
-        const data = await response;
+	try {
+		const response = await fetch(`/persons/deletePerson/${personId}`, {
+			method: 'DELETE',
+		})
+		const data = await response
 
-				// If deleted on person page there is no page to redirect to
-				if(page.pathname.startsWith('/persons/')) {
-        	location.replace('/profile');
-				}
-				else {
-					location.reload();
-				}
-    }catch(err){
-        console.error(err)
-    }
+		// If deleted on person page there is no page to redirect to
+		if (page.pathname.startsWith('/persons/')) {
+			location.replace('/profile')
+		} else {
+			location.reload()
+		}
+	} catch (err) {
+		console.error(err)
+	}
 }
